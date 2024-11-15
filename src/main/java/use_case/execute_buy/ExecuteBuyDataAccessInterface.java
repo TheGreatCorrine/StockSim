059b0d8356799f3fd08facefae23dd8bda2c3bc8
xiangy20 <corrine.xiang@mail.utoronto.ca>
2024-11-14 19:08:23 -0500

    3.1 add some docstrings to the Execute_buy use case and some entities. 3.2 implement all six files under src/main/java/use_case/execute_sell 3.3 fix a bug: accidentally move this file to execute_sell.

---
+++ src/main/java/use_case/execute_buy/ExecuteBuyDataAccessInterface.java
@@ -0,0 +1,9 @@
+package use_case.execute_buy;
+
+import entity.User;
+import utility.exceptions.ValidationException;
+
+public interface ExecuteBuyDataAccessInterface {
+    User getUserWithCredential(String credential) throws ValidationException;
+
+}
